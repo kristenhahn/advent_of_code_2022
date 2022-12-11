865b14b93ccaeebe8382323d55007ffc618cbb67
Kristen Hahn <72356393+kristenhahn@users.noreply.github.com>
2022-12-11 01:51:13 -0600

    Parse input

--- Day_11/Day_11.py
+++ Day_11/Day_11.py
@@ -37,7 +37,53 @@ class Monkey:
 
 # m0 = Monkey(0,[79,98],'old * old',23,2,3)
 
+with open('test_input.txt') as f:
+    data = f.read()
+    lines = data.split('\n')
+# remove leading spaces
+lines = [l.strip() for l in lines]
 
+# build a list of monkeys
+monkeys = {}
+m = 0
+
+# Parse the lines into monkey attributes
+for l in lines:
+    monkey_id = 0
+    items = []
+    if l.startswith('Monkey'):
+        words = l.split(' ')
+        monkey_id = words[1].split(':')[0]
+        monkey_id = int(monkey_id)
+        print(monkey_id)
+    elif l.startswith('Starting'):
+        items = l.split(': ')[1]
+        items = items.split(', ')
+        items = [int(i) for i in items]
+        print(items)
+    elif l.startswith('Operation'):
+        operation = l.split('= ')[1]
+        print(operation)
+    elif l.startswith('Test'):
+        test = l.split(" by ")[1]
+        test = int(test)
+        print(test)
+    elif l.startswith("If true"):
+        true_throw_to = l.split(" monkey ")[1]
+        true_throw_to = int(true_throw_to)
+        print(true_throw_to)
+    elif l.startswith("if false"):
+        false_throw_to = l.split(" monkey ")[1]
+        false_throw_to = int(false_throw_to)
+        print(false_throw_to)
+    elif l == '':
+        monkey_id += 1
+        print(monkey_id)
+
+   
+
+
+    
 
 
 
