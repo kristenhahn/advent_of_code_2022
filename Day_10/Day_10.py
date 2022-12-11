7c1c29ecad091297f0c3d2c8f36ed99397e881cb
Kristen Hahn <72356393+kristenhahn@users.noreply.github.com>
2022-12-10 22:42:39 -0600

    Complete Day 10 Part 2

--- Day_10/Day_10.py
+++ Day_10/Day_10.py
@@ -37,21 +37,26 @@ for l in lines:
         cycle_dict[c]=x
 
 
-# Variable to hold the sum of signal strengths
-sum_signal_strength = 0
+# fill in missing cycles in the dictionsary.
+cycle_dict_keys = list(cycle_dict.keys())
+max_cycle = max(cycle_dict_keys)
 
-# Find signal strentgh for cycles 60, 100, 140, 180, and 220.
-# signal strength = cycle number * x value.
-for c in range(20,221,40):
+# Add a placeholder item 0 so that
+# part 2 works.  Need to look for 
+# a sprite spanning pixels 0-2, 
+# so a 0 key is required in the 
+# dictionary.
 
-    # if this cycle is in the dictionary:
-    if c in cycle_dict.keys():
-        x = cycle_dict[c]
+cycle_dict[0]=0
 
-    # if this cycle is not in the dictionary,
-    # the value hasn't changed since the 
-    # most recent recorded cycle.
-    else:
+# fill in the rest of the missing cycles
+for c in range(2,max_cycle+1):
+
+    if c not in cycle_dict_keys:
+
+        # if this cycle is not in the dictionary,
+        # the value hasn't changed since the 
+        # most recent recorded cycle.
         cycle_not_found = True
 
         # lookup the previous cycle
@@ -63,6 +68,8 @@ for c in range(20,221,40):
             # take the x value from that previous cycle.
             if lookup_c in cycle_dict.keys():
                 x = cycle_dict[lookup_c]
+                # assign the same x value to our missing cycle
+                cycle_dict[c] = x
 
                 # change flag to stop looking backward
                 # through the cycles
@@ -73,6 +80,17 @@ for c in range(20,221,40):
             # if that one is found.
             else:
                 lookup_c -= 1
+
+# Variable to hold the sum of signal strengths
+sum_signal_strength = 0
+
+# Find signal strentgh for cycles 60, 100, 140, 180, and 220.
+# signal strength = cycle number * x value.
+for c in range(20,221,40):
+
+    # # if this cycle is in the dictionary:
+    # if c in cycle_dict.keys():
+    x = cycle_dict[c]
 
     # signal strength = cycle * x value
     strength = c * x
@@ -86,15 +104,26 @@ print(sum_signal_strength)
 # Part 2:
 # https://adventofcode.com/2022/day/10#part2
 
-# for a pixel to be drawn, cycle numnber has to equal x-1, x, or x+1.
+# for a pixel to be drawn, cycle number -1 must equal x-1, x, or x+1.
 
-# Add the cycles missing from the dictionary
+def print_pixels(range_start, range_end):
+    pixels = ''
+    for c in range(range_start, range_end):
+        x = cycle_dict[c]
+        
+        # each row of 40 starts over at 0
+        c_thisrow = c%40
 
-# for c in range(242):
-#     if c not in cycle_dict.keys():
+        if x-1 <= c_thisrow-1 <= x+1:
+            pixels = pixels + r'#'
+        else:
+            pixels = pixels + r'.'
+    print(pixels)
 
 
-
-# print(cycle_dict)
-
-
+print_pixels(1,40)
+print_pixels(41,80)
+print_pixels(81,120)
+print_pixels(121,160)
+print_pixels(161,200)
+print_pixels(201,240)
