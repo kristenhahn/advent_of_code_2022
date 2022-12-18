d0dc560cc8360569fc9fa09c8a56285107b5a1b9
Kristen Hahn <72356393+kristenhahn@users.noreply.github.com>
2022-12-18 09:19:23 -0600

    Complete Day 17 Part 1

--- Day_17/Day_17.py
+++ Day_17/Day_17.py
@@ -6,12 +6,9 @@ class Rock:
         self,
         shape:str,
         base_point:tuple):
-        # chamber_ymax:int,
-        # chamber_width=7):
 
         self.shape = shape
-        # self.chamber_ymax = chamber_ymax
-        # self.chamber_width = chamber_width
+        self.base_point = base_point
 
     # dictionary of shapes with height, width, and occupied squares
     # in x-y format. 
@@ -24,7 +21,7 @@ class Rock:
             'a':{'height':1, 'width':4, 'occupied':{(0,0),(1,0),(2,0),(3,0)}},
             'b':{'height':3, 'width':3, 'occupied':{(1,0),(0,1),(1,1),(2,1),(1,2)}},
             'c':{'height':3, 'width':3, 'occupied':{(0,0),(1,0),(2,0),(2,1),(2,2)}},
-            'd':{'height':4, 'width':1, 'occupied':{(0,0),(0,1),(0,2),(0,3),(0,4)}},
+            'd':{'height':4, 'width':1, 'occupied':{(0,0),(0,1),(0,2),(0,3)}},
             'e':{'height':2, 'width':2, 'occupied':{(0,0),(1,0),(0,1),(1,1)}}
             }
 
@@ -32,23 +29,30 @@ class Rock:
         self.width = self.shape_dict[self.shape]['width']
         self.shape_item =self.shape_dict[self.shape]
         self.occupied_points = self.shape_item['occupied']
+        self.max_y = self.base_point[1] + self.height - 1
         
         # update the occupied points based on the starting point for each
         # newly created shape:
-        # x = 2, y = chamber_ymax + 3
+        # x = 2, y = chamber_ymax + 3. This method seems clunky - is 
+        # there a better way?
         old_points = self.occupied_points.copy()
         self.occupied_points.update([(x+base_point[0],y+base_point[1])for (x,y) in self.occupied_points])
         self.occupied_points.difference_update(old_points)
 
-    def print_shape(self):
-        for r in reversed(range(self.height)):
-            row_text = ''
-            for c in range(self.width):
-                if (c,r) in self.occupied_points:
-                    row_text += '@'
-                else:
-                    row_text += '.'
-            print(row_text)
+
+    def update_max_y(self):
+        self.max_y = self.base_point[1] + self.height - 1
+        return self.max_y
+
+    # def print_shape(self):
+    #     for r in reversed(range(self.height)):
+    #         row_text = ''
+    #         for c in range(self.width):
+    #             if (c,r) in self.occupied_points:
+    #                 row_text += '@'
+    #             else:
+    #                 row_text += '.'
+    #         print(row_text)
 
     def right_points(self):
         right_points = set()
@@ -56,17 +60,17 @@ class Rock:
         return right_points
 
     def move_right(self):
-        new_occupied_points = self.right_points()
         self.occupied_points = self.right_points()
-
+        self.base_point = (self.base_point[0]+1, self.base_point[1])
+  
     def left_points(self):
         left_points = set()
         left_points.update([(x-1,y) for (x, y) in self.occupied_points])
         return left_points
 
     def move_left(self):
-        new_occupied_points = self.left_points()
         self.occupied_points = self.left_points()
+        self.base_point = (self.base_point[0]-1, self.base_point[1])
 
     def down_points(self):
         down_points = set()
@@ -74,26 +78,23 @@ class Rock:
         return down_points
 
     def move_down(self):
-        new_occupied_points = self.down_points()
         self.occupied_points = self.down_points()
+        self.base_point = (self.base_point[0], self.base_point[1]-1)
+        # self.max_y = self.max_y - 1
 
     
-
-
-
 class Chamber():
     
     def __init__(
         self,
-        width=7,
-        max_wall_height=-1,
-        max_rock_height=-1):
+        width=7):
 
         self.width = width
-        self.max_wall_height = max_wall_height
-        self.max_rock_height = max_rock_height
+        self.max_wall_height = -1
 
-        self.occupied_points = {()}
+        self.occupied_points = set()
+
+        self.max_rock_y = -1
 
     # Starting point:  
     #   Column -1 = a wall.
@@ -103,6 +104,7 @@ class Chamber():
     def build_floor(self):
         floor = [(x, -1) for x in range(-1,self.width+1)]
         self.occupied_points.update(floor)
+        self.max_rock_y = -1
 
     # walls
     def add_rows(self,num_rows):
@@ -110,12 +112,6 @@ class Chamber():
             self.max_wall_height += 1
             walls = [(-1,self.max_wall_height),(self.width,self.max_wall_height)]
             self.occupied_points.update(walls)
-
-    # # initial chamber = floor plus row 0
-    # def build_floor(self):
-    #     self.add_floor()
-    #     self.add_rows(1)
-
      
     def print_chamber(self):
         for r in reversed(range(-1,self.max_wall_height+1)):
@@ -128,13 +124,24 @@ class Chamber():
             print(row_text)
         print('')
 
+    def print_chamber_slice(self, min_y, max_y):
+        for r in reversed(range(min_y,max_y)):
+            row_text = ''
+            for c in range(-1,self.width+1):
+                if (c,r) in self.occupied_points:
+                    row_text += '@'
+                else:
+                    row_text += '.'
+            print(row_text)
+        print('')
+        
     def add_rock(self, rock_type):
-        base_point = (2, self.max_rock_height + 4)
+        base_point = (2, self.max_rock_y + 4)
         rock = Rock(rock_type, base_point)
 
         # build chamber walls to the top of the
         # new rock
-        extra_height = rock.height + 3
+        extra_height = rock.base_point[1]+rock.height - self.max_wall_height
         self.add_rows(extra_height)
 
         # record the rock's occupied points in the chamber
@@ -171,7 +178,7 @@ class Chamber():
     def move_rock_down(self, rock:Rock):
         # remove rock points from chamber temporarily
         self.occupied_points.difference_update([p for p in self.occupied_points if p in rock.occupied_points])
-        # if the rock moves one space right, see if there are
+        # if the rock moves one space down, see if there are
         # any obstructions
         overlap = self.occupied_points.intersection(rock.down_points())
         if len(overlap) == 0:
@@ -179,10 +186,101 @@ class Chamber():
             self.occupied_points.update(rock.occupied_points)
         # if there are obstructions, put the rock back where it was.
         else:
-            self.occupied_points.update(rock.occupied_points)    
+            self.occupied_points.update(rock.occupied_points)
+            rock_y = rock.update_max_y()
+            if self.max_rock_y < rock_y:
+                self.max_rock_y = rock_y
+            return ('rock bottom')
 
-        
+###########       
+# Run game
+###########
 
+with open('input.txt') as f:
+    data = f.read()
+
+# remove blanks and separate into list of left-right instructions
+inst = data.strip()
+inst = list(inst)
+
+rock_order = ['a', 'b', 'c', 'd', 'e']
+
+# Set up a new chamber
+chamber = Chamber()
+chamber.build_floor()
+# chamber.print_chamber()
+
+fallen_rocks = 0
+
+max_rock_height = 0
+
+while fallen_rocks < 2022:
+
+    # get the next rock type to fall and add it to the back of the list
+    rock_type = rock_order.pop(0)
+    rock_order.append(rock_type)
+
+    # A new rock appears
+    rock = chamber.add_rock(rock_type)
+
+    ## Test print as needed ##
+    # print_max_y = rock.update_max_y() + 5
+    # print_min_y = max(-1,print_max_y - 12)
+    # print('A new rock appears')
+    # chamber.print_chamber_slice(print_min_y, print_max_y)
+    # print()
+
+    turn_end = False
+
+    while turn_end == False:
+
+        # get the next instruction and add it to the back of the list
+        push = inst.pop(0)
+        inst.append(push)
+
+        # jets push the rock
+        if push == "<":
+            chamber.move_rock_left(rock)
+        elif push == ">":
+            chamber.move_rock_right(rock)
+        else:  print("Error, invalid gas jet instruction")
+
+        ## Test print as needed ##
+        # print(f'\n push {push}')
+        # print_max_y = rock.update_max_y() + 5
+        # print_min_y = max(-1,print_max_y - 12)
+        # chamber.print_chamber_slice(print_min_y, print_max_y)
+        # print()
+
+        # Rock falls one level
+        move = chamber.move_rock_down(rock)
+        if move == 'rock bottom':
+            turn_end = True
+            fallen_rocks += 1
+            # print(f'{fallen_rocks} rocks have fallen.')
+
+        ## Test print as needed
+        # print('Rock falls')
+        # print_max_y = rock.update_max_y() + 5
+        # print_min_y = max(-1,print_max_y - 12)
+        # chamber.print_chamber_slice(print_min_y, print_max_y)
+        # print()
+
+print(f'Total rock height = {chamber.max_rock_y+1}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
@@ -196,35 +294,35 @@ class Chamber():
 
 ### test printing ###
 
-test_chamber = Chamber()
-test_chamber.build_floor()
+# test_chamber = Chamber()
+# test_chamber.build_floor()
 
-r = test_chamber.add_rock('d')
-test_chamber.print_chamber()
-test_chamber.move_rock_right(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_right(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_left(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_down(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_left(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_left(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_left(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_left(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_down(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_down(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_down(r)
-test_chamber.print_chamber()
-test_chamber.move_rock_down(r)
-test_chamber.print_chamber()
+# r = test_chamber.add_rock('d')
+# test_chamber.print_chamber()
+# test_chamber.move_rock_right(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_right(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_left(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_down(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_left(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_left(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_left(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_left(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_down(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_down(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_down(r)
+# test_chamber.print_chamber()
+# test_chamber.move_rock_down(r)
+# test_chamber.print_chamber()
 
 
 
